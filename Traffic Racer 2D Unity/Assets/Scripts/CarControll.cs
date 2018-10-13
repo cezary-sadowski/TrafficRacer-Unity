d8314be09d5bd101e24d8dedfac0e3eccdc49aa8
Cezary <cezary@MacBook-Air-Cezary.local>
2018-10-14 00:44:58 +0200

    CarControllHorizontal

---
+++ Traffic Racer 2D Unity/Assets/Scripts/CarControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControll : MonoBehaviour 
+{
+    public float carHorizontalSpeed;
+
+    private Vector3 _carPosition;
+
+    public void Start()
+    {
+        _carPosition = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        _carPosition.x += Input.GetAxis("Horizontal") * carHorizontalSpeed * Time.deltaTime; //deltaTime real game time.
+
+        _carPosition.x = Mathf.Clamp(_carPosition.x, -2.40f, 2.40f);
+
+        this.gameObject.transform.position = _carPosition;
+    }
+}
